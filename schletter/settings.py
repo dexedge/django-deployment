86ff96ff16df1d24e9c6ca6b2eeeed4a81de9bd4
Dexter <dexedge@gmail.com>
2021-04-01 19:57:58 -0400

    And another heroku fix

--- schletter/settings.py
+++ schletter/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'pi
+STATIC_URL = '/static/'
 
 # Heroku settings
 django_heroku.settings(locals())
