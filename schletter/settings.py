3eb1f7d4768f68bb955efe5442d4eec02cb86aea
Dexter <dexedge@gmail.com>
2021-04-09 13:20:52 -0400

    Style index page

--- schletter/settings.py
+++ schletter/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'schletter.urls'
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static',]
 
 # Heroku settings
 django_heroku.settings(locals())
