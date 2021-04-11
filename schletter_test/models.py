f94bbd297f1acc122d9fb3330a97ea030830424d
Dexter <dexedge@gmail.com>
2021-04-11 13:44:26 -0400

    pre-heroku commit

--- schletter_test/models.py
+++ schletter_test/models.py
@@ -22,7 +22,7 @@ class Event(models.Model):
     notes = models.TextField(blank=True)
 
     class Meta:
-        ordering = ['theater', 'title']
+        ordering = ['date', 'theater', 'title']
     
     def __str__(self):
         event = str(self.date) + ", " + self.title
