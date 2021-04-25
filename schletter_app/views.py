2856fe0cc5b5c7fd0cc07ca2a90119607aa94e7c
Dexter <dexedge@gmail.com>
2021-04-25 19:41:57 -0400

    Initial commit on prev-next feature

--- schletter_app/views.py
+++ schletter_app/views.py
@@ -60,23 +60,46 @@ class EventDetail(DetailView):
         context['events'] = Event.objects.all()
         return context
 
-class WorkList(ListView):
+class WorkQueryMixin:
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query is not None:
+            return Work.objects.filter(Q(title__icontains=query) | Q(genre__icontains=query))
+        else:
+            return Work.objects.all()
+
+class WorkList(WorkQueryMixin, ListView):
     context_object_name = 'works'
     model = Work
     paginate_by = 50
     template_name = 'schletter_app/works.html'
 
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query is not None:
-            return Work.objects.filter(Q(title__icontains=query))
-        else:
-            return Work.objects.all()
-
-class WorkDetail(DetailView):
+class WorkDetail(WorkQueryMixin, DetailView):
     context_object_name = 'work'
     model = Work
     template_name = 'schletter_app/work.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        prev_pk = (
+            self.get_queryset()
+            .filter(sort_title__lt=self.object.sort_title)
+        .reverse().values('pk')[:1]
+        )
+        # There may be no next page
+        if prev_pk:
+            context['prev_pk'] = prev_pk[0]['pk']
+        
+        next_pk = (
+            self.get_queryset()
+            .filter(sort_title__gt=self.object.sort_title)
+            .values('pk').values('pk')[:1]
+        )
+        # There may be no next page
+        if next_pk:
+            context['next_pk'] = next_pk[0]['pk']
+        
+        return context
 
 class AuthorList(ListView):
     context_object_name = 'authors'
@@ -90,8 +113,7 @@ class AuthorList(ListView):
             return Author.objects.filter(Q(last_name__icontains=query) | Q(first_names__icontains=query))
         else:
             return Author.objects.all()
-    
-    
+
 class AuthorDetail(DetailView):
     context_object_name = 'author'
     model = Author
