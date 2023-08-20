6e078ebe2dbbaf2d9e71b3f6363b94aacca0542e
Dexter <dexedge@gmail.com>
2023-08-20 14:08:47 -0400

    Hardcode storage bucket name

--- schletter/settings.py
+++ schletter/settings.py
@@ -152,7 +152,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID =  os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_STORAGE_BUCKET_NAME = "schletter"
+# os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 
 AWS_QUERYSTRING_AUTH = False 
 # This will make sure that the file URL does not have unnecessary parameters like your access key.
@@ -166,10 +167,12 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = 'staticfiles'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_FINDERS = (
 'django.contrib.staticfiles.finders.FileSystemFinder',
 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
 
 # ckeditor settings
 # The 'divarea' plugin preserves the site styles
