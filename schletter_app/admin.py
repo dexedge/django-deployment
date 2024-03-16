ba2b6909333027c91058a5ca7b79bcceed72c33d
Dexter <dexedge@gmail.com>
2024-03-16 10:34:25 -0400

    Add sort, search, filter to Admin on main tables

--- schletter_app/admin.py
+++ schletter_app/admin.py
@@ -4,15 +4,38 @@ from django.contrib import admin
 from .models import Date, Event, WorkEvent, Work, Author, AuthorWork, Composer, ComposerWork
 from adminsortable2.admin import SortableAdminMixin
 
-admin.site.register(Date)
+class DateAdmin(admin.ModelAdmin):
+    ordering = ('date',)
+    date_hierarchy = 'date'
+
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre')
+    ordering = ('title', 'genre')
+    list_filter = ('genre',)
+    search_fields = ('title',)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_names', 'birth', 'death')
+    ordering = ('last_name', 'birth', 'death')
+    search_fields = ('last_name',)
+
+class ComposerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_names', 'birth', 'death')
+    ordering = ('last_name', 'birth', 'death')
+    search_fields = ('last_name',)
+
+admin.site.register(Date, DateAdmin)
 # admin.site.register(Event)
 admin.site.register(WorkEvent)
-admin.site.register(Work)
-admin.site.register(Author)
+admin.site.register(Work, WorkAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(AuthorWork)
-admin.site.register(Composer)
+admin.site.register(Composer, ComposerAdmin)
 admin.site.register(ComposerWork)
 
 @admin.register(Event)
 class SortableEventAdmin(SortableAdminMixin, admin.ModelAdmin):
-    pass
+   list_display = ('date', 'title', 'theater', 'company')
+   list_filter = ('theater', 'company')
+   search_fields = ('title',)
+
